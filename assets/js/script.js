d96400a44ca924d16ea18e8a1c77a2c462c888f5
anlianaaguilar <anlianaaguilar@gmail.com>
2022-05-16 21:53:17 -0400

    funcion validar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 // document ready event
 //capturar el evento boton
 $(function(){
-    $("#buscar").click(()=>{
-        alert("soy boton")
+    $("#button").click(()=>{
+        //bucar el personaje
+        buscarPersonaje()
     })
+
 })
+
+function buscarPersonaje(){ 
+    let personaje = $("#info_input").val()//captura info del input
+    alert("buscar personaje" + personaje)
+    //validar que info_input solo sera numeros
+    alert(validar(personaje))
+    //buscar personajes
+}
+
+function validar(num){
+    let expression = /^\d{1,3}$/  //acepta de 1 a 3 digitos (regexr.com)
+    if(expression.test(num)){  //se evalua con test y retorna true o false
+        return true
+    }else{
+        return false
+    }
+}
